a15511339d4d1f76163d67657d00f8a6d762d7ec
phamcaohung <phamcao77@gmail.com>
2024-02-18 21:21:15 +0700

    change axio to api

--- src/state/product/Action.js
+++ src/state/product/Action.js
@@ -1,5 +1,4 @@
-import axios from "axios"
-import { API_BASE_URL, api } from "../../config/apiConfig"
+import { api } from "../../config/apiConfig"
 import { CREATE_PRODUCT_FAILURE, CREATE_PRODUCT_REQUEST, CREATE_PRODUCT_SUCCESS, DELETE_PRODUCT_FAILURE, DELETE_PRODUCT_REQUEST, DELETE_PRODUCT_SUCCESS, FIND_PRODUCT_BY_ID_FAILURE, FIND_PRODUCT_BY_ID_REQUEST, FIND_PRODUCT_BY_ID_SUCCESS, FIND_PRODUCT_FAILURE, FIND_PRODUCT_REQUEST, FIND_PRODUCT_SUCCESS, UPDATE_PRODUCT_FAILURE, UPDATE_PRODUCT_REQUEST, UPDATE_PRODUCT_SUCCESS, FIND_PRODUCT_BY_SERIES_REQUEST, FIND_PRODUCT_BY_SERIES_SUCCESS, FIND_PRODUCT_BY_SERIES_FAILURE, FIND_CAROUSEL_PRODUCT_BY_SERIES_REQUEST, FIND_CAROUSEL_PRODUCT_BY_SERIES_SUCCESS, FIND_CAROUSEL_PRODUCT_BY_SERIES_FAILURE, GET_ALL_SERIES_REQUEST, GET_ALL_SERIES_SUCCESS, GET_ALL_SERIES_FAILURE } from "./ActionType"
 
 export const  findProducts = (reqData) => async (dispatch) => {
@@ -18,9 +17,7 @@ export const  findProducts = (reqData) => async (dispatch) => {
         pageNumber, 
         pageSize } = reqData
     try {
-        // const { data } = await api.get(`/api/products?tier=${tier}&minPrice=${minPrice}&maxPrice=${maxPrice}
-        // &minDiscount=${minDiscount}&category=${category}&name=${name}&series=${series}&stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`)
-        const { data } = await axios.get(`${API_BASE_URL}/api/products?tier=${tier}&minPrice=${minPrice}&maxPrice=${maxPrice}
+        const { data } = await api.get(`/api/products?tier=${tier}&minPrice=${minPrice}&maxPrice=${maxPrice}
         &minDiscount=${minDiscount}&category=${category}&name=${name}&series=${series}&stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`)
         
         dispatch({ type: FIND_PRODUCT_SUCCESS, payload: data })
@@ -47,8 +44,7 @@ export const createProduct = (product) => async (dispatch) => {
     dispatch({ type: CREATE_PRODUCT_REQUEST})
 
     try {
-        //const { data } = await api.post(`/api/admin/products/`, product)
-        const { data } = await axios.post(`${API_BASE_URL}/api/admin/products/`, product)
+        const { data } = await api.post(`/api/admin/products/`, product)
 
         dispatch({
             type: CREATE_PRODUCT_SUCCESS,
