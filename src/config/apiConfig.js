2d7b550558cddbe2fa09641f603c14a2d38189e1
phamcaohung <phamcao77@gmail.com>
2024-02-20 16:08:27 +0700

    change api

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 //export const API_BASE_URL = "http://localhost:5454"
-export const API_BASE_URL = "https://balmy-dog-production.up.railway.app"
+export const API_BASE_URL = "https://endurable-stone-production.up.railway.app"
 
 const jwt = localStorage.getItem("jwt")
 
